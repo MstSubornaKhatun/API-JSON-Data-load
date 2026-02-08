ec1490164c0382644fdce6933f30b4adfa8a38c0
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-08 09:34:22 +0600

    api

--- script.js
+++ script.js
@@ -2,9 +2,19 @@
 // console.log(result) // Promise {<pending>}
 
 
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(json => console.log(json)) // {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
 
-      
+
+
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json)) // btn a click korle --> {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+
+}
+
+
+
+
