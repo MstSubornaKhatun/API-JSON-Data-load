0402d0110d6e93eb186195f2de892bf2b03786c5
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-08 09:16:14 +0600

    api json data

---
+++ script.js
@@ -0,0 +1,10 @@
+// const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
+// console.log(result) // Promise {<pending>}
+
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json)) // {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+
+      
